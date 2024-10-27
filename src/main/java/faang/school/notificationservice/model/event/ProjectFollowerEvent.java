47c1ebf1d3c049e5e529541bd307d6c0586b349c
Aleh Biahun <stones2875@gmail.com>
2024-10-27 17:17:36 +0100

    Adding the annotation - @JsonIgnoreProperties(ignoreUnknown = true) in the classes - ProjectFollowerEvent and UserFollowerEvent

--- src/main/java/faang/school/notificationservice/model/event/ProjectFollowerEvent.java
+++ src/main/java/faang/school/notificationservice/model/event/ProjectFollowerEvent.java
@@ -1,8 +1,10 @@
 package faang.school.notificationservice.model.event;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 @Setter
 public class ProjectFollowerEvent {
