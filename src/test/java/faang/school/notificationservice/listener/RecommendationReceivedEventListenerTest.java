58a284f32a87e6c6fc6e1198de03ab5dd74909c6
azhibek_a <azhibek.assylkhan@gmail.com>
2024-10-28 14:14:59 +0500

    fix import

--- src/test/java/faang/school/notificationservice/listener/RecommendationReceivedEventListenerTest.java
+++ src/test/java/faang/school/notificationservice/listener/RecommendationReceivedEventListenerTest.java
@@ -26,8 +26,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyLong;
 
 @Slf4j
 @ExtendWith(MockitoExtension.class)
