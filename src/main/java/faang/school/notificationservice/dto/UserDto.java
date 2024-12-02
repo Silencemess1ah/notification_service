4d510621a37e0d03e86dd7be184ef781fda6a31a
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-12-02 22:38:42 +0300

    completing the task of sending email

--- src/main/java/faang/school/notificationservice/dto/UserDto.java
+++ src/main/java/faang/school/notificationservice/dto/UserDto.java
@@ -1,12 +1,18 @@
 package faang.school.notificationservice.dto;
 
+import jakarta.validation.constraints.Email;
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 public class UserDto {
     private long id;
     private String username;
+
+    @Email
     private String email;
+
     private String phone;
     private PreferredContact preference;
 
