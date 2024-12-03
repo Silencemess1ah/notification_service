6bb750bb532f082ac16c61facba12540ecd977a1
Denis Levkhov <levkhovde@yandex.ru>
2024-12-04 00:34:45 +0300

    BSJ2-41782: Adding retry

--- src/main/java/faang/school/notificationservice/service/TelegramService.java
+++ src/main/java/faang/school/notificationservice/service/TelegramService.java
@@ -3,8 +3,11 @@ package faang.school.notificationservice.service;
 import faang.school.notificationservice.bot.TelegramBotImpl;
 import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.exception.TelegramBotInitException;
+import faang.school.notificationservice.exception.TelegramBotMessageSendException;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -22,9 +25,10 @@ public class TelegramService implements NotificationService {
     }
 
     @PostConstruct
+    @Retryable(retryFor = TelegramBotInitException.class, backoff = @Backoff(delay = 30000, multiplier = 2))
     public void init() {
         try {
-            log.debug("Initializing Telegram bot");
+            log.debug("Trying to initializing Telegram bot");
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(telegramBot);
             log.info("Telegram bot initialized with name: {}", telegramBot.getBotUsername());
@@ -35,6 +39,7 @@ public class TelegramService implements NotificationService {
     }
 
     @Override
+    @Retryable(retryFor = TelegramBotMessageSendException.class, backoff = @Backoff(delay = 60000, multiplier = 2))
     public void send(UserDto user, String message) {
         log.debug("Sending message to user #{} in Telegram: {}", user.getId(), message);
         SendMessage sendMessage = SendMessage.builder()
@@ -46,7 +51,7 @@ public class TelegramService implements NotificationService {
             log.info("Message sent to user #{} in Telegram: {}", user.getId(), message);
         } catch (TelegramApiException e) {
             log.error("Failed to send message to Telegram", e);
-            throw new TelegramBotInitException("Failed to send message to Telegram: " + e.getMessage());
+            throw new TelegramBotMessageSendException("Failed to send message to Telegram: " + e.getMessage());
         }
     }
 
