0b4828565240203698c51fa6f78572aacfdf74fa
Maxim Gorobets <gorobets.maks@bk.ru>
2024-12-03 21:42:43 +1000

    done without tests

---
+++ src/main/java/faang/school/notificationservice/config/context/telegram/MessageRequest.java
@@ -0,0 +1,13 @@
+package faang.school.notificationservice.config.context.telegram;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MessageRequest {
+
+    private String message;
+}
--- src/main/java/faang/school/notificationservice/service/NotificationService.java
+++ src/main/java/faang/school/notificationservice/service/NotificationService.java
@@ -1,9 +1,10 @@
 package faang.school.notificationservice.service;
 
 import faang.school.notificationservice.dto.UserDto;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public interface NotificationService {
-    void send(UserDto user, String message);
+    void send(UserDto user, String message) throws TelegramApiException;
     UserDto.PreferredContact getPreferredContact();
 
 }
