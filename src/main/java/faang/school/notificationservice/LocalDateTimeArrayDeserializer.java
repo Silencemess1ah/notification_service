8d858a8036ff1847be0b9197feaa33028c1682d4
SerUKalinin <serukalinin@gmail.com>
2024-12-04 00:43:44 +0300

    Доработка сервиса LocalDateTimeArrayDeserializer

--- src/main/java/faang/school/notificationservice/LocalDateTimeArrayDeserializer.java
+++ src/main/java/faang/school/notificationservice/LocalDateTimeArrayDeserializer.java
@@ -11,9 +11,9 @@ public class LocalDateTimeArrayDeserializer extends JsonDeserializer<LocalDateTi
     public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         int[] dateTimeArray = p.readValueAs(int[].class);
         return LocalDateTime.of(
-            dateTimeArray[0], dateTimeArray[1], dateTimeArray[2], // Год, месяц, день
-            dateTimeArray[3], dateTimeArray[4], dateTimeArray[5], // Часы, минуты, секунды
-            dateTimeArray[6]                                      // Наносекунды
+            dateTimeArray[0], dateTimeArray[1], dateTimeArray[2],
+            dateTimeArray[3], dateTimeArray[4], dateTimeArray[5],
+            dateTimeArray[6]
         );
     }
 }
