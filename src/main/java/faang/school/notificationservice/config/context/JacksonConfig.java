34c802a1899a9d80c75130e4e4d6b7875070d33e
SerUKalinin <serukalinin@gmail.com>
2024-12-04 00:43:34 +0300

    Доработка сервиса JacksonConfig

--- src/main/java/faang/school/notificationservice/config/context/JacksonConfig.java
+++ src/main/java/faang/school/notificationservice/config/context/JacksonConfig.java
@@ -3,17 +3,26 @@ package faang.school.notificationservice.config.context;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class JacksonConfig {
 
     @Bean(name = "customObjectMapper")
     public ObjectMapper objectMapper() {
+        log.info("Настройка ObjectMapper с поддержкой Java Time API.");
+
         ObjectMapper objectMapper = new ObjectMapper();
+
         objectMapper.registerModule(new JavaTimeModule());
+        log.info("Модуль JavaTimeModule зарегистрирован.");
+
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        log.info("Даты будут сериализованы в ISO-8601 формате, без использования timestamps.");
+
         return objectMapper;
     }
 }
