0b99c2c952d2f7d358c72b5e8e8c415e1cbba272
Maxim Gorobets <gorobets.maks@bk.ru>
2024-12-05 23:05:05 +1000

    explicit port and host for redis config

--- src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
@@ -1,10 +1,12 @@
-package faang.school.notificationservice.redis;
+package faang.school.notificationservice.config.redis;
 
 import faang.school.notificationservice.listener.RedisContainerMessageListener;
 import faang.school.notificationservice.listener.impl.MentorshipAcceptedEventListener;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
@@ -16,9 +18,16 @@ public class RedisConfig {
 
     private final List<RedisContainerMessageListener> listeners;
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
