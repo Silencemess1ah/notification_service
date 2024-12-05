35c7a655dc829ec2033b815e981a6e10814aa9bf
Maxim Gorobets <gorobets.maks@bk.ru>
2024-12-05 14:14:36 +1000

    small fix

--- src/main/java/faang/school/notificationservice/redis/RedisConfig.java
+++ src/main/java/faang/school/notificationservice/redis/RedisConfig.java
@@ -1,13 +1,20 @@
 package faang.school.notificationservice.redis;
 
+import faang.school.notificationservice.listener.RedisContainerMessageListener;
 import faang.school.notificationservice.listener.impl.MentorshipAcceptedEventListener;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
+
+    private final List<RedisContainerMessageListener> listeners;
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
@@ -18,7 +25,7 @@ public class RedisConfig {
     RedisMessageListenerContainer redisContainer(MentorshipAcceptedEventListener mentorshipAcceptedEventListener) {
         RedisMessageListenerContainer redisContainer = new RedisMessageListenerContainer();
         redisContainer.setConnectionFactory(jedisConnectionFactory());
-        redisContainer.addMessageListener(mentorshipAcceptedEventListener.getAdapter(), mentorshipAcceptedEventListener.getTopic());
+        listeners.forEach(listener -> redisContainer.addMessageListener(listener.getAdapter(), listener.getTopic()));
         return redisContainer;
     }
 }
