086e4c4100f547b60648a9a9c0a56046e0c34032
Alexander Litovkin <litovkinalexan@gmail.com>
2024-12-05 11:59:05 +0700

    add redis config

---
+++ src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
@@ -0,0 +1,51 @@
+package faang.school.notificationservice.config.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.channel.achievement}")
+    private String achievementTopic;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration =
+                new RedisStandaloneConfiguration(redisHost, redisPort);
+
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    MessageListenerAdapter achievementMessageListener() {
+        return new MessageListenerAdapter((MessageListener) (message, pattern) -> {});
+    }
+
+    @Bean
+    ChannelTopic achievementTopic() {
+        return new ChannelTopic(achievementTopic);
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisContainer() {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(jedisConnectionFactory());
+        container.addMessageListener(achievementMessageListener(), achievementTopic());
+
+        return container;
+    }
+}
