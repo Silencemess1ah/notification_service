208cf7597bbfd6403a217fb9060aaf8a5629016e
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-12-05 15:28:14 +0300

    BJS2-41866 Обработчик событий

--- src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
@@ -3,22 +3,39 @@ package faang.school.notificationservice.listener;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.exception.EventHandlingException;
 import faang.school.notificationservice.messaging.MessageBuilder;
 import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
+@Slf4j
 @RequiredArgsConstructor
-public abstract class AbstractEventListener<T>{
+public abstract class AbstractEventListener<T> {
 
     private final ObjectMapper objectMapper;
     private final UserServiceClient userServiceClient;
     private final List<NotificationService> notificationServices;
     private final List<MessageBuilder<T>> messageBuilders;
 
-
+    protected void handleEvent(Message message, Class<T> eventClass, Consumer<T> consumer) {
+        try {
+            T event = objectMapper.readValue(message.getBody(), eventClass);
+            log.info("Received event: {}", event);
+            consumer.accept(event);
+        } catch (IOException e) {
+            log.error("Error while handling {} ({}) from Redis message"
+                    , eventClass, new String(message.getBody()), e);
+            throw new EventHandlingException
+                    ("Error while handling " + eventClass.getName() + " from Redis message");
+        }
+    }
 
     protected String getMessage(T event, Locale locale){
         return messageBuilders.stream()
